af3b8386a11149a177d915ac05aa98933e6bff55
Ryo Hayasaka <ryohysk@gmail.com>
2019-10-17 00:28:27 +0900

    resolve #6 Domaを導入する

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 	id("io.spring.dependency-management") version "1.0.8.RELEASE"
 	kotlin("jvm") version "1.3.50"
 	kotlin("plugin.spring") version "1.3.50"
+	kotlin("kapt") version "1.3.50"
 	id("org.flywaydb.flyway") version "5.2.4"
 }
 
@@ -33,9 +34,29 @@ dependencies {
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	implementation("com.graphql-java-kickstart:graphql-spring-boot-starter:5.10.0")
 	runtimeOnly("com.graphql-java-kickstart:altair-spring-boot-starter:5.10.0")
+	kapt("org.seasar.doma:doma:latest.release")
+	implementation("org.seasar.doma:doma:latest.release")
+	implementation("org.seasar.doma.boot:doma-spring-boot-starter:latest.release")
+}
+
+val compileKotlin: KotlinCompile by tasks
+
+kapt {
+	arguments {
+		arg("doma.resources.dir", compileKotlin.destinationDir)
+	}
+}
+
+tasks.register("copyDomaResources", Sync::class) {
+	from(sourceSets.main.get().resources.srcDirs)
+	into(compileKotlin.destinationDir)
+	include("doma.compile.config")
+	include("META-INF/**/*.sql")
+	include("META-INF/**/*.script")
 }
 
 tasks.withType<KotlinCompile> {
+	dependsOn(tasks.getByName("copyDomaResources"))
 	kotlinOptions {
 		freeCompilerArgs = listOf("-Xjsr305=strict")
 		jvmTarget = "1.8"
